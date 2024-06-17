fb0c27a8a1df78a4c13984792968ccd9f0d4cbd6
Mickael Seguin <mickael_seguin@hotmail.fr>
2024-06-17 09:35:59 +0200

    images saved locally

--- public/javascripts/format-images.js
+++ public/javascripts/format-images.js
@@ -2,31 +2,65 @@ const sharp = require('sharp')
 const fs = require('fs');
 const path = require('path');
 
-const directoryPath = path.join(__dirname, '../images/vanilla');
+const directoryPath = path.join(__dirname, '../images');
 
+const treatImages = (image, name) => {
 
-// // Lister tous les fichiers en utilisant forEach
-// files.forEach(function (file) {
-//     console.log(file);
-// });
+    return new Promise((resolve, reject) => {
+        let width = 0;
+        let height = 0;
 
-const formatImage = (image,width,height,format) => {
-sharp(image) // specify the path to the image
-    .resize(width, height) // resize the image to 200x200 pixels
-    .toFormat(format)
-    .toBuffer('output.jpg', (err,data, info) => { // save the output to 'output.jpg'
-        if (err) {
-            console.error('Error occurred', err);
-        } else {
-fs.writeFile(`output.${format}`, data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});
-            console.log(data)
-            console.log('Image resized successfully', info);
-        }
+        sharp(image)
+            .metadata()
+            .then(function (metadata) {
+                if(metadata.width < 600 || metadata.height < 600) {
+                    width = metadata.width;
+                    height = metadata.height;
+                }else{
+                    if (metadata.width > metadata.height) {
+                        height = 600;
+                        width = Math.floor((600 * metadata.width) / metadata.height);
+                    } else {
+                        width = 600;
+                        height = Math.floor((600 * metadata.height) / metadata.width);
+                    }
+                }
+
+                return sharp(image)
+                    .resize(width, height)
+                    .jpeg({
+                        quality: 80,
+                        mozjpeg: false,
+                        chromaSubsampling: '4:4:4'
+                    })
+                    .toBuffer()
+            })
+            .then(data => {
+                console.log(data)
+                // return data;
+                fs.writeFile(path.join(directoryPath, `${name}.jpg`), data, (err) => {
+                    if (err) reject(err);
+                    else {
+                        console.log('The file has been saved!');
+                        resolve();
+                    }
+                });
+            })
+            .catch(err => {
+                console.error('Error occurred', err);
+                reject(err);
+            });
     });
 }
 
-formatImage('../images/vanilla/Moody1.jpg',200,200,'avif')
+const formatImages = (images) => {
+    console.log(images)
+    const imagePromises = images.map((image, index) => {
+        return treatImages(image.buffer, image.originalname.split('.')[0]);
+    });
+
+    return Promise.all(imagePromises);
+}
+
+module.exports = formatImages;
 
--- routes/image.js
+++ routes/image.js
@@ -5,18 +5,21 @@ const path = require('path')
 const multer = require('multer')
 const upload = multer({storage: multer.memoryStorage()})
 
+const formatImages = require('../public/javascripts/format-images.js');
+const outputDir = path.join(__dirname, '../images/');
 
-router.post('/', upload.single('image'), function(req, res, next) {
-    // Assume that req.body.image is a base64-encoded image
-    if (!req.file) {
-        res.status(400).send('No image provided');
+router.post('/', upload.array('image'), async (req, res, next) => {
+    if (!req.files || req.files.length === 0) {
+        res.status(400).send('No images provided');
         return;
     }
 
-    console.log(req.file.buffer)
-
-    res.send('Received image');
-
+    try {
+        await formatImages(req.files);
+        res.send('Received and formatted images');
+    } catch (err) {
+        res.status(500).send('Error occurred while formatting images');
+    }
 });
 
 
